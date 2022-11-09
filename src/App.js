ae76485d95ca1d9ac6ac08c7a6b2f6e2f2e2dce5
R3moulade <91252465+R3moulade@users.noreply.github.com>
2022-11-09 12:46:58 +0100

    imported jeffs code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
+import Places from './components/Places'
 
 import Home from "./pages/Home";
 import Calendar from "./pages/Calendar"
@@ -10,6 +11,11 @@ import Explore from "./pages/Explore"
 function App() {
   return (
     <div>
+      <h1>Where should we eat?</h1>
+          <div className="container">
+            <Places/>
+          </div>
+          
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
