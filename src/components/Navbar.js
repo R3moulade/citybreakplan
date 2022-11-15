bb2b9a76137f6a1d648944ecaccd7f44f37491a1
toldeveloper <89986144+toldeveloper@users.noreply.github.com>
2022-11-15 15:17:15 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,30 @@
+import "../App.css"
 import { NavLink } from 'react-router-dom';
 
-// Icons
-import HomeIcon from '@mui/icons-material/Home';
-import EventIcon from '@mui/icons-material/Event';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import AccessibleForwardIcon from '@mui/icons-material/AccessibleForward';
+import ExploreIcon from '@mui/icons-material/ExploreOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import CategoryIcon from '@mui/icons-material/CategoryOutlined';
+import CalendarTodayIcon from '@mui/icons-material/CalendarTodayOutlined';
 
-// CSS
-import styles from './Navbar.module.css'
+export default function NavBar () {
 
-export default function Navbar() {
-    return (
+return(
 
-        <div>
-          <nav className={styles.nav1}>
-            <NavLink to="/home" end>
-              <HomeIcon />
-            </NavLink>
-            <NavLink to="/calendar">
-              <EventIcon />
-            </NavLink> 
-            <NavLink to="/explore">
-              <AccessibleForwardIcon />
-            </NavLink>
-            <NavLink to="/settings">
-              <MoreVertIcon />
-            </NavLink>
-          </nav>
-        </div>
+  <nav id="nav" className='nav'>
+    <NavLink to="/explore" end>
+      <ExploreIcon/>
+    </NavLink>
+    <NavLink to="/categories">
+      <CategoryIcon/>
+    </NavLink>
+    <NavLink to="/calender">
+      <FavoriteBorderOutlinedIcon/>
+    </NavLink>
+    <NavLink to="/more">
+      <CalendarTodayIcon/>
+    </NavLink>
+  </nav>
+)
 
-    )
+
 }
